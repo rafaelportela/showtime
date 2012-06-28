fc312c4c4d45d82c600b604e029df62843763e8c
rafaelportela <rafaelportela90@gmail.com>
2012-06-27 23:32:44 -0300

    rspec and cucumber setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Showtime::Application.config.secret_token = 'a12b19812369fe098b41afbecc7bf1cd4c484d42bc4ebe1d8458983b91dfb63b13373703098d8255468b5b60cce3f3d6ab3554e7e5d46ce7adcca5b94fb0f113'
