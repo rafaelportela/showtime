18aa869c9468819425d87ba9a877c7920adb8bbf
rafaelportela <rafaelportela90@gmail.com>
2012-06-28 00:34:24 -0300

    feature Showtime Descriptions passed

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20120628032722) do
+
+  create_table "movies", :force => true do |t|
+    t.date     "showtime_date"
+    t.time     "showtime_time"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
 
 end
